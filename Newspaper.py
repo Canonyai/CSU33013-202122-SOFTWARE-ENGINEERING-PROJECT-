52d7ef7bf40a02d209098accb8de54bac2dd45cc
PradyumnBhardwaj <74086671+PradyumnBhardwaj@users.noreply.github.com>
2022-03-22 16:35:18 +0000

    Update Newspaper.py
    Summariser working

--- Newspaper.py
+++ Newspaper.py
@@ -1,6 +1,8 @@
 
 from newspaper import Article
 import newspaper
+import nltk
+nltk.download('punkt')
 
 URLS = ['https://www.latimes.com/world-nation/story/2020-09-20/coronavirus-aerosol-airborne-spread',
         'https://edition.cnn.com/2020/12/22/asia/hong-kong-pirates-eli-boggs-intl-hnk-dst/index.html'
@@ -21,5 +23,8 @@ newspaper.news_pool.join()
 for i in sources:
     i.articles[0].download()
     i.articles[0].parse()
+    i.articles[0].nlp()
+
     print('Title:', i.articles[0].title)
     print('Author:', i.articles[0].authors)
+    print('Summary:', i.articles[0].summary)
