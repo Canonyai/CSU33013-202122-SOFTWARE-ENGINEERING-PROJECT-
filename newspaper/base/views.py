1c8f037f494789d2280e3803b28c4b0c87d600dd
Kinjal-16 <bhattacharyyakinjal@gmail.com>
2022-04-03 17:30:30 +0100

    Adding the django framework

--- newspaper/base/views.py
+++ newspaper/base/views.py
@@ -1,12 +1,94 @@
 from django.shortcuts import render
 # Create your views here.
 import pandas as pd
-import pymongo
+import pymongo  # for mongodb access
+import pandas as pd
+import requests
 
 conn = "mongodb://localhost:27017"
 client = pymongo.MongoClient(conn)
 
 # Create a database
+def putData():
+    
+# Establish connection
+    conn = "mongodb://localhost:27017"
+    client = pymongo.MongoClient(conn)
+
+# Import the csv files
+    db = client.Headlines
+    business = pd.read_csv('business.csv')
+    entertainment = pd.read_csv('entertainment.csv')
+    general=pd.read_csv('general.csv')
+    health=pd.read_csv('health.csv')
+    science=pd.read_csv('science.csv')
+    sports=pd.read_csv('sports.csv')
+    technology=pd.read_csv('technology.csv')
+
+    data = business.to_dict(orient='records')
+    db.business.insert_many(data)
+
+    data = entertainment.to_dict(orient='records')
+    db.entertainment.insert_many(data)
+
+    data = general.to_dict(orient='records')
+    db.general.insert_many(data)
+
+    data = health.to_dict(orient='records')
+    db.health.insert_many(data)
+
+    data = science.to_dict(orient='records')
+    db.science.insert_many(data)
+
+    data = sports.to_dict(orient='records')
+    db.sports.insert_many(data)
+
+    data = technology.to_dict(orient='records')
+    db.technology.insert_many(data)
+
+
+categories = ["business", "entertainment", "general", "health", "science", "sports", "technology"]
+for list in categories:
+
+    url = ('http://newsapi.org/v2/top-headlines?'
+           'country=us&'
+           'category=' + list + '&'
+                                'language=en&'
+                                'apiKey=a6514dbbe8bb4c559910baeaf5c2a848')
+    urlResponse = requests.get(url)
+    urlResponse = urlResponse.json()
+
+    articles = []
+    for item in urlResponse['articles']:
+        dict = {
+
+            "title": item['title'],
+            "urlToImage": item['urlToImage'],
+            "description": item['description']
+        }
+        articles = articles + [dict]
+
+    conn = "mongodb://localhost:27017"
+    client = pymongo.MongoClient(conn)
+
+    if len(articles) > 0:
+        df = pd.DataFrame(urlResponse['articles'])
+        df = df.loc[:, ["title", "urlToImage"]]
+        df.to_csv(list + '.csv')
+conn = "mongodb://localhost:27017"
+client = pymongo.MongoClient(conn)
+
+# Create a database
+db = client.Headlines
+
+db.business.delete_many({})
+db.entertainment.delete_many({})
+db.general.delete_many({})
+db.health.delete_many({})
+db.science.delete_many({})
+db.sports.delete_many({})
+db.technology.delete_many({})
+putData()
 db = client.Headlines
 
 hG = pd.DataFrame(db.general.find())
@@ -91,3 +173,5 @@ def home(request):
     return render(request,'home.html',{'general_headlines':general,'entertainment_headlines':Entertainment,'business':business,'health':health,'science':science,'sports':sports,'technology':technology})
 def summarize(request,pk):
     return render(request,'summarize.html')
+
+
